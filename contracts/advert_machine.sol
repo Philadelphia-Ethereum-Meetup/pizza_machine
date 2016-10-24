215624c22cc2c7306c9e662977363453e196e5f0
tjayrush <jrush@greathill.com>
2016-10-23 22:39:30 -0400

    First stab at advert contract

--- contracts/advert_machine.sol
+++ contracts/advert_machine.sol
@@ -1,35 +1,70 @@
+// Advertising Machine
+//
+// The advertising machine allows the sponsor to make several settings related
+// to how the viewer will consume the advertising such a list of urls to
+// distribute to the viewer. Additionally, such things as a list of questions
+// to ask the viewer after consuming the advertising, what types of information
+// to ask of the user, etc.
 contract advert_machine
 {
-	// This solidity file is curated by the advertiser. The advertiser
-	// decides how many videos the participants must watch. The
-	// advertiser decides how long the video is, and if they want to
-	// to ask for certain required information such as name and email.
+	// While the contract is deployed by the 'host' of the group,
+	// the machine is assigned to a curator, who would normally be
+	// the sponsor. The sponse makes all settings related to how
+	// the contract operates.
 	//
-	// If the viewer meets the demands of the contract, the contract
-	// issues a time-sensitive token. That token much be 'notarized'
-	// by the viewer and return to the pizza_machine for verification.
-	//
-	// State:
-	string videos[];
+	// state variables:
+	string urls[];
+	uint nURLs;
 	struct View {
-		string name;
-		string email;
-		string url;
-		uint timestamp;
+		string viewer_email;
+		string url_consumed;
+		uint when_consumed;
 	};
 	View views[];
-	bool name_required;
-	bool email_required;
+	bool email__req;
 
-	function advert_machine(string video, bool nm_req, bool em_req)
-	{
-		videoURL = video;
-		name_required = nm_req;
-		email_required = em_req;
+	modifier isAdmin() {
+		if (msg.sender != admin)
+			throw;
+		_
 	}
 
-	function show_me_a_video(string name, string email)
+	function advert_machine()
 	{
 	}
 
+	function pickURL() return string {
+		return urls[block.timestamp%nURLs]
+	}
+
+	function addURL(string url) isAdmin() {
+		urls[nUrls++]=url;
+	}
+
+	function removeURL(string url, uint n) isAdmin() {
+		if (urls[n] == url)
+			urls[n]="";
+		cleaupURLs();
+	}
+
+	function cleanupURLs() internal {
+		uint cnt=0;
+		for (i=0;i<nURLs;i++) {
+			if (urls[i]!="")
+				urls[cnt++] = urls[i];
+		}
+		nUrls=cnt;
+	}
+
+	function require_email(bool req) {
+		email_req = req;
+	}
+
+	function watchedURL(string url, string email) returns hash {
+		if (!findURL(url))
+			throw;
+		if (email_req && email.isempty())
+			throw;
+		return sha256(url+email+timestamp);
+	}
 }
